4e06b39189db301d6ad5eb67f4d49827051e2399
devin.c.north <devin.c.north@accenture.com>
2020-01-06 14:00:13 -0600

    Completed functions practice part two

---
+++ HelloKotlin/src/FunctionsPracticeTwo.kt
@@ -0,0 +1,36 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+  var fortune: String
+  for (i in 1..10) {
+    fortune = getFortune(getBirthday())
+    println("\nYour fortune is: $fortune")
+    if (fortune.contains("Take it easy")) break;
+  }
+}
+
+fun getFortune(birthday: Int): String {
+  val fortunes = listOf(
+      "You will have a great day!",
+      "Things will go well for you today.",
+      "Enjoy a wonderful day of success.",
+      "Be humble and all will turn out well.",
+      "Today is a good day for exercising restraint.",
+      "Take it easy and enjoy life!",
+      "Treasure your friends because they are your greatest fortune."
+  )
+  return when {
+    birthday in 1..7 ->
+      "Your birthday is in the first week... you are MEGA DOOMED. Man, sucks to be you."
+    birthday == 28 || birthday == 31 ->
+      "Your birthday is on the 28th or 31st... you are DOOMED. Sorry, them's the breaks kiddo."
+    birthday <= 0 || birthday >= 32 ->
+      "ERROR: NON-EXISTENT BIRTH DATE, ANOMALIES WILL BE TERMINATED"
+    else -> fortunes[birthday % fortunes.size]
+  }
+}
+
+fun getBirthday(): Int {
+  println("Enter the numerical date of your birth (just day portion, i.e. 8/9/1997, enter 8):")
+  return readLine()?.toIntOrNull() ?: Random().nextInt(32)
+}
--- HelloKotlin/src/Goldfish.kt
+++ HelloKotlin/src/Goldfish.kt
@@ -1,16 +1,29 @@
 import java.util.Random
 
 fun main(args: Array<String>) {
+  for (i in 1..100) {
     feedTheFish()
+  }
 }
 
 fun feedTheFish() {
-    val day = randomDay()
-    val food = "Pellets"
-    println("Today is $day and the fish eat $food")
+  val day = randomDay()
+  val food = fishFood(day)
+  println("Today is $day and the fish ${if (food === "fasting") "are $food" else "eat $food"}")
 }
 
 fun randomDay(): String {
-    val week = listOf("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-    return week[Random().nextInt(7)]
+  val week = listOf("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+  return week[Random().nextInt(7)]
+}
+
+fun fishFood(day: String): String {
+  return when (day) {
+    "Sunday" -> "plankton"
+    "Monday" -> "flakes"
+    "Wednesday" -> "red worms"
+    "Thursday" -> "granules"
+    "Friday" -> "mosquitoes"
+    else -> "fasting"
+  }
 }
